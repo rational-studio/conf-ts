d1610ef411484e06c0bfa09a3b94fd09c2b8234d
Zhongliang Wang <zwang@rational.studio>
2025-09-02 00:07:30 +0800

    add property access test case for arrayMap macro

--- packages/@conf-ts/compiler/tests/macros/array-map.conf.ts
+++ packages/@conf-ts/compiler/tests/macros/array-map.conf.ts
@@ -1,8 +1,20 @@
 import { arrayMap } from '@conf-ts/macro';
 
 const nums = [1, 2, 3];
+const objects = [
+  { id: 1, name: 'Alice' },
+  { id: 2, name: 'Bob' },
+  { id: 3, name: 'Charlie' },
+  { id: 4, name: 'David' },
+  { id: 5, name: 'Eve' },
+];
 
 export default {
   mapDouble: arrayMap(nums, x => (x > 1 ? x * 2 : x)),
   mapToString: arrayMap(nums, x => (x > 1 ? `${x}` : 'one')),
+  mapNames: arrayMap(objects, obj => obj.name),
+  alternateObjects: arrayMap(objects, obj => ({
+    ...obj,
+    ...(obj.name === 'Charlie' ? { isAdmin: true } : {}),
+  })),
 };
--- packages/@conf-ts/compiler/tests/specs/property-access.conf.ts
+++ packages/@conf-ts/compiler/tests/specs/property-access.conf.ts
@@ -8,6 +8,7 @@ export default {
   host: config.host,
   port: config.port,
   'test-1': { cool: 123, awesome: true },
+  // prettier-ignore
   "test-2": { cool: 456, awesome: false },
   [key]: { cool: 789, awesome: true },
   [`test-${key}`]: { cool: 101112, awesome: false },
